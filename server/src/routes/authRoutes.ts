a78adbd64ef47e7949c3b74528d8a184bffc7124
Adil <adilzafar66@gmail.com>
2023-07-05 22:55:32 -0700

    Add signout route

--- server/src/routes/authRoutes.ts
+++ server/src/routes/authRoutes.ts
@@ -1,9 +1,10 @@
 import express from 'express';
-import authController from '../controllers/authentication';
+import authController from '../controllers/auth';
 
 const router = express.Router();
 
 router.post('/signup', authController.postSignup);
 router.post('/signin', authController.postSignin);
+router.post('/signout', authController.postSignout);
 
 export = router;
